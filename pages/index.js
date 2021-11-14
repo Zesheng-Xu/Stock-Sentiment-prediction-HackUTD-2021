412718fc3829ceaa3aeccf820b253e162ba9a125
saudbaig456 <saudsamibaig@gmail.com>
2021-11-13 18:41:02 -0600

    Working Search

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,29 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import SearchableSelect from '../components/SearchableSelect'
 import React, { useState } from 'react'
+import companyNames from '../data/names.json'
+import tickers from '../data/tickers.json'
+import tickerLookup from '../data/name.json'
 
-const options = [
+/*const options = [
   { value: 'chocolate', label: 'Chocolate' },
   { value: 'strawberry', label: 'Strawberry' },
   { value: 'vanilla', label: 'Vanilla' }
-]
+]*/
+
 
 
 export default function Home() {
 
-  const [value, setValue] = useState("chocolate")
+  const [value, setValue] = useState("")
+  const [type, setType] = useState("ticker")
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    console.log(`Submitting ${value}`)
+    const submitVal = value
+    if(type == "name") {
+      submitVal = tickerLookup[value]["Symbol"]
+    }
     //will handle submission of user's selected ticker
   }
 
@@ -25,14 +33,26 @@ export default function Home() {
     setValue(e.value)
   }
 
+  const handleTypeChange = e => {
+    setType(e.target.value)
+  }
+
   return (
     <div className={styles.container}>
       <form
-      className={styles.tickerForm}
-       onSubmit={handleSubmit}>
+        className={styles.tickerForm}
+        onSubmit={handleSubmit}>
+        
+        <label for="searchType">Ticker or Name: </label>
+        <select name="search" id="searchType" onChange={handleTypeChange}>
+         <option value="ticker">Ticker</option>
+         <option value="name">Company Name</option>
+        </select>
 
         <SearchableSelect
-          options={options}
+          options={
+            (type == "ticker" ? tickers : companyNames)
+          }
           handleChange={handleChange}
         />
         <button className={styles.submitButton}>Submit</button>
