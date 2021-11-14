dab225126e1a59275215fc29a58bbcb9cb1236a0
TwentyPorts <2rayxu+github@gmail.com>
2021-11-13 22:00:36 -0600

    Push to firebase

--- pages/results.js
+++ pages/results.js
@@ -1,25 +1,31 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 import { db } from "../firebase";
-import Link from 'next/link'
+import Link from "next/link";
 
 // Get a list of companies from your database
 async function getCompanies() {
   var companiesRef = db.ref("companies");
-  companiesRef.on("value", (snapshot) => {
+  /*companiesRef.on("value", (snapshot) => {
     const data = snapshot.val();
     console.log(data);
+  });*/
+  companiesRef.child("AMZN").set({
+    Influence: 0,
+    NACIS: 0,
+    'Public Opinion': 0,
+    Stock: 0
   });
 }
 
 const rec = "BUY";
 
-export default function Results(props) {
+export default function Results() {
   return (
     <div id={getCompanies()}>
-      <h2 className={styles.nameTicker}>Name/TICKER {props.ticker}</h2>
+      <h2 className={styles.nameTicker}>Name/TICKER</h2>
       <h1 className={rec == "BUY" ? styles.buy : styles.sell}>{rec}</h1>
       <p className={styles.infoHeader}>Stock Price</p>
       <p className={styles.infoBody}>$50.00</p>
@@ -29,7 +35,9 @@ export default function Results(props) {
       <p className={styles.infoBody}>1T</p>
       <p className={styles.infoHeader}>Sentiment/Social Media</p>
       <p className={styles.infoBody}>Positive</p>
-      <Link href="./"><a>Back</a></Link>
+      <Link href="./">
+        <a>Back</a>
+      </Link>
     </div>
   );
 }
